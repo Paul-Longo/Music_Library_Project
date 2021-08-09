0679f78ddfca45ca276c0ecf13f070c11a5c8360
Paul-Longo <imanazarite88@gmail.com>
2021-08-09 12:45:14 -0400

    created a path for the likes class in the music_app/views.py

--- music_library/music_app/urls.py
+++ music_library/music_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('music_app/', views.SongList.as_view()),
     path('music_app/<int:pk>', views.SongDetail.as_view()),
+    path('music_app/<int:pk>likes', views.SongLikes.as_view())
 ]
