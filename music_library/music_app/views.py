a79fad7aec373f978cb5ec57921c37f5f2acb2ee
Paul-Longo <imanazarite88@gmail.com>
2021-08-09 17:32:23 -0400

    updated put method and delete method

--- music_library/music_app/views.py
+++ music_library/music_app/views.py
@@ -1,19 +1,19 @@
-from django.http.response import Http404
 from django.shortcuts import render
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http.response import Http404
 
 # Create your views here.
 
-#Show a list of songs with an option to create a new song to be added to the list.
+# Show a list of songs with an option to create a new song to be added to the list.
 class SongList(APIView):
 
     def get(self, request):
-        song = Song.objects.all()
-        serializer = SongSerializer(song, many=True)
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
 
     def post(self, request):
@@ -24,6 +24,8 @@ class SongList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Retreive details, update details, or delete details of a song.
+
+
 class SongDetail(APIView):
 
     def get_object(self, pk):
@@ -42,16 +44,20 @@ class SongDetail(APIView):
         song = self.get_object(pk)
         serializer = SongSerializer(song, data=request.data)
         if serializer.is_valid():
+            serializer.update(song, request.data)
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         song = self.get_object(pk)
+        serializer = SongSerializer(song)
         song.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.data)
+
 
 class SongLikes(APIView):
-    
+
     def get(self, request, pk):
         song = self.get_objects(pk)
         song.likes += 1
