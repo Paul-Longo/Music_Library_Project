e8d817cba5c0302b9eb0d9d36a8c279448170270
Paul-Longo <imanazarite88@gmail.com>
2021-08-09 11:51:58 -0400

    added a path to song detail in urls.py in music app

--- music_library/music_app/urls.py
+++ music_library/music_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('music/', views.SongList.as_view()),
+    path('music_app/', views.SongList.as_view()),
+    path('music_app/<int:pk>', views.SongDetail.as_view()),
 ]
