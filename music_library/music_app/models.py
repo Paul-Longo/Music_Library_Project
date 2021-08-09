a4dab4679d63b7590069a8fb5c2625f4bf59b61e
Paul-Longo <imanazarite88@gmail.com>
2021-08-09 17:33:54 -0400

    updated the Song class release_date and likes null=true

--- music_library/music_app/models.py
+++ music_library/music_app/models.py
@@ -7,4 +7,5 @@ class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
-    release_date = models.DateTimeField()
+    release_date = models.DateTimeField(null=True)
+    likes = models.IntegerField(null=True)
