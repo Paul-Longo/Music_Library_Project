5ebcdea3ad66e32946d1e6771d5677eaebd43471
Paul-Longo <imanazarite88@gmail.com>
2021-08-13 15:50:45 -0400

    changed music_app folder to music. added django corsheaders

--- music_library/music_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('music_app/', views.SongList.as_view()),
-    path('music_app/<int:pk>', views.SongDetail.as_view()),
-    path('music_app/<int:pk>likes', views.SongLikes.as_view()),
-]
--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music_app'
+    'music',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'music_library.urls'
@@ -117,3 +119,5 @@ try:
     from music_library.local_settings import *
 except ImportError:
     pass
+
+CORS_ORIGIN_ALLOW_ALL = True
